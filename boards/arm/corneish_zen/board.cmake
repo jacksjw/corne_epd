103c397dfe71495af8219b7e9129d9267d9495a9
1218 <98438916+a741725193@users.noreply.github.com>
2024-05-20 11:46:12 +0800

    Update board.cmake

--- boards/arm/corneish_zen/board.cmake
+++ boards/arm/corneish_zen/board.cmake
@@ -1,5 +1,11 @@
 # SPDX-License-Identifier: MIT
 
+# board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
+# include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
+# include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+
+
+set(OPENOCD_NRF5_SUBFAMILY nrf52)
 board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
-include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
